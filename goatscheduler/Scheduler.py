d258728b38069b59e795d4bc25255942edb8b034
vinny <vinnymeller@gmail.com>
2021-02-08 22:09:35 -0600

    updated scheduler.py

--- goatscheduler/Scheduler.py
+++ goatscheduler/Scheduler.py
@@ -50,38 +50,45 @@ class Scheduler():
         for task in self.tasks:
             task.refresh_state()
 
-    def _run_ready_tasks(self):
-        print(f'Preparing to run ready tasks')
+    
+    def kill_dead_threads(self):
         thread_idx_to_kill = []
         for i, task_thread in enumerate(self.task_threads):
-            if not task_thread.is_alive():
+            if not task_thread['thread'].is_alive():
                 thread_idx_to_kill.append(i)
-        for i in sorted(thread_idx_to_kill, reverse=True): # delete references to dicts from the worker list
+        for i in sorted(thread_idx_to_kill, reverse=True):
+            print(f'Finished task {self.task_threads[i]["task"].name}')
             del self.task_threads[i]
-        
+    
+    def run_ready_tasks(self):
+        print(f'Preparing to run ready tasks')
         i = 0
         while len(self.task_threads) < self.max_threads and i < len(self.tasks):
             if self.tasks[i].state['ready']:
+                new_task_thread = {
+                    'task': self.tasks[i],
+                    'thread': threading.Thread(target=self.tasks[i].run)
+                }
+                self.task_threads.append(new_task_thread)
                 print(f'Starting task {self.tasks[i].name}')
-                self.task_threads.append(threading.Thread(target=self.tasks[i].run))
-                self.task_threads[-1].start()
+                self.task_threads[-1]['thread'].start()
             i += 1
         print(f'Ran all possible ready tasks')
 
-    
+
+    def refresh_states(self):
+        self._refresh_schedule_states()
+        self._refresh_task_states()
+
     def start(
         self,
-        refresh_secs: int = 10,
-        max_threads: int = 3
+        refresh_secs: int = 5
     ):
         print('Starting scheduler')
         while(True):        # loop 4eva
-            self._refresh_schedule_states()
-            self._refresh_task_states()
-            self._run_ready_tasks()
-
-            # set ready tasks to run
-            print(f'Sleeping for {refresh_secs} seconds')
+            self.refresh_states()
+            self.kill_dead_threads()
+            self.run_ready_tasks()
             time.sleep(refresh_secs)    # wait to check again
         
 
